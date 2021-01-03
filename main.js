b620edd10e2f36c626533fbc2e6110b8df06a8ab
Gopi Srinivasan <igopisrinivasan@gmail.com>
2021-01-03 16:04:31 +0530

    menu added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 let mainWindow;
 
 //Listen for app to be ready
@@ -19,8 +19,68 @@ app.on("ready", function () {
       slashes: true,
     })
   );
+
+  //Quit app when closed
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
+
+  //Build menu from template
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  //Insert MEnu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+//Handle create add window
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 150,
+    title: "Add Shopping List Item",
+  });
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+
+  //Garbage Collection handle
+  addWindow.on("close", () => {
+    addWindow = null;
+  });
+}
 
 //Create menu template
 
-const mainMenuTemplate = [];
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Items",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Clear Items",
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+//if mac, add empty object to menu
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
