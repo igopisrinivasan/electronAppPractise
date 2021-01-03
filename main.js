03a2710837affc8465000451b70b642daea9299e
Gopi Srinivasan <igopisrinivasan@gmail.com>
2021-01-03 19:53:26 +0530

    issue need to be fixed

--- main.js
+++ main.js
@@ -1,73 +1,83 @@
 const electron = require("electron");
-const url = require("url");
 const path = require("path");
+const url = require("url");
 
-const { app, BrowserWindow, Menu } = electron;
+// SET ENV
+process.env.NODE_ENV = "development";
+
+const { app, BrowserWindow, Menu, ipcMain, ipcRenderer } = electron;
+
 let mainWindow;
+let addWindow;
 
-//Listen for app to be ready
-
+// Listen for app to be ready
 app.on("ready", function () {
-  //Create new window
+  // Create new window
   mainWindow = new BrowserWindow({});
-  //Load html into window
-
+  // Load html in window
   mainWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, "mainWindow.html"),
-      protocol: "file",
+      protocol: "file:",
       slashes: true,
     })
   );
-
-  //Quit app when closed
+  // Quit app when closed
   mainWindow.on("closed", function () {
     app.quit();
   });
 
-  //Build menu from template
-
+  // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
-  //Insert MEnu
+  // Insert menu
   Menu.setApplicationMenu(mainMenu);
 });
 
-//Handle create add window
+// Handle add item window
 function createAddWindow() {
   addWindow = new BrowserWindow({
     width: 300,
-    height: 150,
+    height: 200,
     title: "Add Shopping List Item",
   });
   addWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, "addWindow.html"),
-      protocol: "file",
+      protocol: "file:",
       slashes: true,
     })
   );
-
-  //Garbage Collection handle
-  addWindow.on("close", () => {
+  // Handle garbage collection
+  addWindow.on("close", function () {
     addWindow = null;
   });
 }
 
-//Create menu template
+// Catch item:add
+ipcMain.on("item:add", function (e, item) {
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  //addWindow = null;
+});
 
+// Create menu template
 const mainMenuTemplate = [
+  // Each object is a dropdown
   {
     label: "File",
     submenu: [
       {
-        label: "Add Items",
+        label: "Add Item",
         click() {
           createAddWindow();
         },
       },
       {
         label: "Clear Items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        },
       },
       {
         label: "Quit",
@@ -80,7 +90,26 @@ const mainMenuTemplate = [
   },
 ];
 
-//if mac, add empty object to menu
+// If OSX, add empty object to menu
 if (process.platform == "darwin") {
   mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        role: "reload",
+      },
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  });
 }
